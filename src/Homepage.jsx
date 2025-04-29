b99c5e30ba1556e244969edacb4c8eee891448d7
PierreLouisBouchez <pl.bouchez@outlook.fr>
2025-04-30 00:14:59 +0200

    Bomberman + lego webm + amélioration écran details

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,79 +1,96 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const cards = [
-    { title: "Make Your Stand x Squeegee", image: "mys.png" },
-    { title: "Aigle", image: "aigle.png" },
-    { title: "Lego", image: "lego.png" },
-    { title: "Aigle", image: "aigle.png" },
-    { title: "Aigle", image: "aigle.png" },
-    { title: "Aigle", image: "aigle.png" },
-    { title: "Aigle", image: "aigle.png" },
-    { title: "Aigle", image: "aigle.png" },
+  { title: "Bomberman", image: "bomberman.png" , video: "bomberman.webm" },
+  { title: "Briqueton", image: "lego.png", video: "Lego.webm" },
+  { title: "Make Your Stand x Squeegee", image: "mys.png" },
+  { title: "Aigle", image: "aigle.png" },
+  { title: "Aigle", image: "aigle.png" },
+  { title: "Aigle", image: "aigle.png" },
+  { title: "Aigle", image: "aigle.png" },
+  { title: "Aigle", image: "aigle.png" },
 
-    { title: "Aigle", image: "aigle.png" },
-    { title: null, image: "" },
+  { title: "Aigle", image: "aigle.png" },
+  { title: null, image: "" },
 
-  ];
-  
-  const HoverCard = ({ project, handler }) => {
-    const base = import.meta.env.BASE_URL;
+];
 
-    return (
-      
-  <div     className="relative select-none bg-center bg-cover bg-no-repeat min-h-86 h-2/3  min-w-64 w-92 outline-4 outline-gris overflow-hidden shadow-abberationrelief translate-y-6 "   onClick={()=>{handler(project.title)}}>
-    
-    {project.title && <img
-          src={`${base}/projects/${project.image}`}
-          alt={project.title}
-          className="object-cover object-top  w-full h-full"
-          loading="lazy"
-        />}
-    <div className="absolute inset-0 hover:bg-black/60 transition duration-300 flex items-center justify-center opacity-0 hover:opacity-100  hover:translate-z-24">
-      <p className="text-white text-lg font-bold">{project.title}</p>
-    </div>
-  </div>
+const HoverCard = ({ project, handler }) => {
+  const base = import.meta.env.BASE_URL;
+  const videoRef = useRef(null);
 
-
-
-    );
+  const handleMouseEnter = () => {
+    videoRef.current.play();
   };
-  
+
+  const handleMouseLeave = () => {
+    videoRef.current.pause();
+    videoRef.current.currentTime = 0; // Optionnel : remet à 0
+  };
+  return (
+
+    <div className="relative select-none  min-h-86 h-2/3  min-w-64 w-92 outline-2 border-4 border-gris outline-gris overflow-hidden shadow-abberationrelief translate-y-6 " onClick={() => { handler(project.title) }}>
+
+      {project.title && <img
+        src={`${base}/projects/${project.image}`}
+        alt={project.title}
+        className="absolute object-cover   w-full h-full"
+        loading="lazy"
+      />}
+      <div className="absolute inset-0 duration-300 flex items-end justify-center opacity-0 hover:opacity-100 cursor-pointer transition-opacity  hover:translate-z-24">
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="absolute h-full w-full object-cover">
+          <video ref={videoRef} src={`${base}/videos/${project.video}`} className="h-full object-cover" muted loop playsInline />
+        </div>
+        <div className="absolute h-full w-full bg-[linear-gradient(to_top,_rgba(0,0,0,0.8)_0%,_rgba(0,0,0,0.2)_10%,_transparent_100%)] pointer-events-none"></div>
+        <p className="absolute text-white text-2xl pb-2 font-bold transition">{project.title}</p>
+      </div>
+    </div>
+
+
+
+  );
+};
+
 
 export default function Projects() {
 
-    const [currentProject,setCurrentProject]=useState(null)
+  const [currentProject, setCurrentProject] = useState(null)
 
-    const handleClick=(id)=>{
-      console.log(id);      
-      
-      setCurrentProject(currentProject==id?null:id);
-    };
+  const handleClick = (id) => {
+    console.log(id);
 
-    return (
-      <div className=" flex-1 max-w-2/3 perspective-normal perspective-origin-top-left  h-100vh overflow-hidden">
-        <div className="absolute h-[100%] w-[95%]  translate-x-3 -translate-3 rotate-x-[2deg] rotate-y-[1deg] transform-3d justify-center overflow-hidden p-4">
+    setCurrentProject(currentProject == id ? null : id);
+  };
+
+  return (
+    <div className=" flex-1 max-w-2/3 perspective-normal perspective-origin-top-left  h-100vh overflow-hidden">
+      <div className="absolute h-[100%] w-[95%]  translate-x-3 -translate-3 rotate-x-[2deg] rotate-y-[1deg] transform-3d justify-center overflow-hidden p-4">
         {(
-          <div className="w-full h-full p-6 border-4 border-t-8 border-r-8 border-gris shadow-abberation bg-[#ababab] ">
-            <div className="w-full h-full border-4 border-t-8 border-r-8 border-gris overflow-hidden bg-radial from-green-950 from-20% to-[#101010]">
-              <div className="text-white flex h-16 text-5xl inset-y-15 right-16 absolute  cursor-pointer text-shadow-neonblanc  " onClick={()=>{setCurrentProject(null)}}>{"-->"}</div>
+          <div className="w-full h-full p-4 border-4 border-t-8 border-r-8 border-gris shadow-abberation bg-[#ababab] ">
+            <div className="absolute h-full w-full bg-[url('/Images/Grid.png')] bg-center bg-cover opacity-30"></div>
+
+            <div className="w-full h-full border-4 border-t-8 border-r-8 p-8 border-blur border-gris overflow-hidden bg-radial from-green-950 from-20% to-[#101010]">
+              <div className="h-full w-full border-6 border-white flex shadow-neonblanc ">
+
+              <div className="text-white flex h-16 text-5xl inset-y-32 right-32 absolute cursor-pointer text-shadow-neonblanc  " onClick={() => { setCurrentProject(null) }}>{"-->"}</div>
               <div className="p-6 text-6xl select-none text-shadow-neonblanc">
                 {currentProject}
-                </div>
+              </div>
+              </div>
             </div>
           </div>
         )}
 
-        </div>
-        <div id='liste' className={`${currentProject!=null?"translate-x-full border-l-[20px] ":"none border-l-4"} w-[95%] bg-[#ababab] border-gris shadow-abberation transition-transform duration-500 right-0 items-right h-[103%]  translate-x-2 -translate-4 ] rotate-x-[2deg] rotate-y-[1deg]  transform-3d justify-center overflow-y-scroll p-4 grid  2xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1   gap-6`}>
-          {cards.map((card, index) => (
-            <HoverCard
+      </div>
+      <div id='liste' className={`${currentProject != null ? "translate-x-full border-l-[20px] " : "none border-l-4"} w-[95%] bg-[#ababab] border-gris shadow-abberation transition-transform duration-500 right-0 items-right h-[103%]  translate-x-2 -translate-4 ] rotate-x-[2deg] rotate-y-[1deg]  transform-3d justify-center overflow-y-scroll p-4 grid  2xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1   gap-6`}>
+        {cards.map((card, index) => (
+          <HoverCard
             key={index}
             project={card}
             handler={handleClick}
-            />
-          ))}
-        </div>
+          />
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
