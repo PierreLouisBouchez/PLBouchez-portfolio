50cb568eafde93f4180ee4bf7529947c5c91c60b
Squeegeeverse <46439687+PierreLouisBouchez@users.noreply.github.com>
2025-12-15 10:10:53 +0100

    Update projects.js

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -41,56 +41,6 @@ export const ListProject = [
     "Description":"Projet de fin de Master réalisé en duo. Le projet était de reproduire le jeu Bomberman sur Unreal Engine de zéro, le défi principal fut de créer un mode multijoueur local."
   },
   {
-    "title":"Make Your Stand",
-    "image":"mys.webp",
-    "logo":"unreal.webp",
-    "techno":[
-      "Unreal Engine",
-      "ReactJS",
-      "Blender",
-      "Pixel Streaming"
-    ],
-    "Link":"https://makeyourstand.fr/",
-    "Description":"Projet réalisé entre 2024 et 2025 pour l'entreprise Make Your Stand. Le but du projet était de créer un configurateur de stand modulaire, pour permettre la création de plusieurs milliers de configuration, le tout grâce à la technologie Pixel Streaming pour proposer une qualité d'image supérieur."
-  },
-  {
-    "title":"Aigle",
-    "image":"aigle.webp",
-    "logo":"react.webp",
-    "techno":[
-      "Unreal Engine",
-      "ReactJS",
-      "Pixel Streaming"
-    ],
-    "Gallery":[
-      "aigle1.webp",
-      "aigle2.webp",
-      "aigle3.webp",
-      "aigle4.webp",
-      "aigle5.webp"
-    ],
-    "Description":"Projet de configurateur de parkas modulaires pour la marque Aigle, la combinaison d'une application web et d'un application Unreal Engine en Pixel streaming à permis de tirer la meilleur partie de chaque technologie.  ",
-    "Link":"https://www.aigle.com/fr/fr/t-kit-module3d.html"
-  },
-  {
-    "title":"Restootab",
-    "image":"restootab.webp",
-    "video":"restootab.webm",
-    "logo":"react.webp",
-    "techno":[
-      "Blender",
-      "ReactJS",
-      "Shopify"
-    ],
-    "Gallery":[
-      "restootab0.avif",
-      "restootab1.avif",
-      "restootab2.avif"
-    ],
-    "Link":"https://www.restootab.com/pages/configurateur",
-    "Description":"Projet réalisé en 2025 pour l'entreprise Restootab. Le but du projet était de créer un configurateur de tables de restaurant, permettant aux clients de personnaliser leur tables et de la commander directement en ligne. Le plus gros défi étant de génerer les plateaux de maniere procédural pour toutes le dimensions et formes disponibles"
-  },
-  {
     "title":"Ville Renouvelée",
     "image":"roubaix.webp",
     "video":"Roubaix.webm",
