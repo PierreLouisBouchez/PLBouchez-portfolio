c24ccd9f6d2520ce9c1f2c7b7bd265d748fc1fee
Squeegeeverse <46439687+PierreLouisBouchez@users.noreply.github.com>
2026-02-23 16:35:37 +0100

    Order Menu

--- src/MockUpBook/MockUpHome.jsx
+++ src/MockUpBook/MockUpHome.jsx
@@ -19,10 +19,10 @@ function MockUpHome() {
     const [bookInfos, setbookInfos] = useState({ depth: 0.2, width: 1, height: 1.5 });
 
     return (
-        <div className='h-[100vh] md:flex-row flex-col flex bg-[#eeeeee] border-[#613a00] text-gray-800 font-Victor '>
-            <div className='p-4 md:w-1/3 w-full '>
+        <div className='h-[100vh] md:flex-row flex-col order flex bg-[#eeeeee] border-[#613a00] text-gray-800 font-Victor '>
+            <div className='p-4 md:w-1/3 w-full order-1 '>
                 <div className='flex flex-col h-full p-4 border-gray-800 border-2 gap-y-2  bg-[#ababab]'>
-                    <h1 className='relative   font-bold pb-2 text-shadow-black text-shadow-xs '>Mock A Book</h1>
+                    <h1 className='relative md:text-5xl! text-xl!  font-bold pb-2 text-shadow-black text-shadow-xs '>Mock A Book</h1>
                     <div className='h-auto bg-gray-300 w-full flex-col flex p-2 border-2'>
                         <span>Depth : {(bookInfos?.depth * 10).toFixed(2)} cm </span>
                         <input className='py-2  h-full justify-items-center rounded-none  ' type='range' min={0.05} max={1} step={0.01} value={bookInfos?.depth} onChange={(e) => setbookInfos({ ...bookInfos, depth: e.target.value })} />
@@ -49,7 +49,7 @@ function MockUpHome() {
                     </div>
                 </div>
             </div>
-            <Canvas className="h-full md:w-2/3 w-full" shadows camera={{ position: [1, 2, 1], fov: 60 }} >
+            <Canvas className="h-full md:w-2/3 w-full md:order-2 order-0" shadows camera={{ position: [1, 2, 1], fov: 60 }} >
                 <hemisphereLight intensity={0.75} color={0xffff88} />
                 <directionalLight position={[-1, 4, 2]} intensity={2} />
                 <Book infos={bookInfos} />
